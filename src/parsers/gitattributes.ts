006f18879e50f557cc8540f180c7c1813d1d6795
Edwin Kofler <edwin@kofler.dev>
2022-06-15 20:10:18 -0700

    refactor: Move gitattributes parser to new parser dir

---
+++ src/parsers/gitattributes.ts
@@ -0,0 +1,111 @@
+import { fs, asserts } from "../deps.ts";
+
+import * as util from "../util/util.ts";
+import * as types from "../types.ts";
+
+export type GitAttributeLine = {
+	comment?: string;
+	newline?: true;
+	pattern?: string;
+	attributes?: Record<string, boolean | string>;
+};
+
+export function parseGitattributes(text: string) {
+	const lines: GitAttributeLine[] = [];
+
+	for (const line of text.split("\n")) {
+		if (line.length == 0) {
+			lines.push({ newline: true });
+			continue;
+		}
+
+		if (line.at(0) === "#") {
+			lines.push({ comment: line });
+		} else {
+			const trimmedLine = line.trim();
+			if (trimmedLine.at(0) === '"') {
+				util.die("C-style commented patterns are not supported"); // FIXME
+			}
+			const [pattern, ...attributes] = trimmedLine
+				.replace(/\s+/g, " ")
+				.split(" ");
+			const attrs: Record<string, boolean | string> = {};
+			for (const attribute of attributes) {
+				if (attribute.includes("=")) {
+					const pair = attribute.split("=");
+					attrs[pair[0]] = pair[1];
+				} else if (attribute.at(0) == "-") {
+					attrs[attribute.slice(1)] = false;
+				} else {
+					attrs[attribute] = true;
+				}
+			}
+			lines.push({ pattern, attributes: attrs });
+		}
+	}
+
+	return lines;
+}
+
+export function separateGitattributes(
+	lines: GitAttributeLine[],
+	problems: types.LintRule[]
+) {
+	const foxxyAttributes: GitAttributeLine[] = [];
+	const otherAttributes: GitAttributeLine[] = [];
+
+	let mode: "default" | "take-foxomate" = "default";
+	for (const line of lines) {
+		if (line.comment) {
+			if (mode === "default") {
+				if (line.comment === "# foxomate start") {
+					mode = "take-foxomate";
+				} else if (line.comment === "# foxomate end") {
+					problems.push({
+						name: "bad-block",
+						description:
+							"End declaration cannot come before starting declaration",
+					});
+				} else if (line.comment === "# foxxy start") {
+					mode = "take-foxomate";
+				} else if (line.comment === "# foxxy end") {
+					problems.push({
+						name: "bad-block",
+						description:
+							"End declaration cannot come before starting declaration",
+					});
+				} else {
+					otherAttributes.push(line);
+				}
+			} else if (mode === "take-foxomate") {
+				if (line.comment === "# foxomate end") {
+					mode = "default";
+				} else if (line.comment === "# foxxy end") {
+					mode = "default";
+				} else {
+					foxxyAttributes.push(line);
+				}
+			}
+		} else if (line.pattern && line.attributes) {
+			if (mode === "default") {
+				otherAttributes.push(line);
+			} else if (mode === "take-foxomate") {
+				foxxyAttributes.push(line);
+			}
+		} else if (line.newline) {
+			if (mode === "default") {
+				otherAttributes.push(line);
+			} else if (mode === "take-foxomate") {
+				foxxyAttributes.push(line);
+			}
+		} else {
+			throw new Error("An attribute unaccounted for was detected");
+		}
+	}
+
+	return { foxxyAttributes, otherAttributes };
+}
+
+function hasFoxomateDeclaration(line: string, value = "") {
+	return line.match(new RegExp(`^(?:#|//)[ \t]*foxomate[ \t]*${value}`));
+}
