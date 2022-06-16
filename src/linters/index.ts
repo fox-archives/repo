9ee247c374ed246f6afed3636597083b9d466f70
Edwin Kofler <edwin@kofler.dev>
2022-06-15 20:06:58 -0700

    wip: Move Old linter code to new location

--- src/linters/index.ts
+++ src/linters/index.ts
@@ -1,15 +1,17 @@
-import bakeModule from "../linters/bake.ts";
-import basaltModule from "../linters/basalt.ts";
-import denoModule from "../linters/deno.ts";
+import bakeModule from "./bake.ts";
+import basaltModule from "./basalt.ts";
+import bashModule from "./bash.ts";
+import denoModule from "./deno.ts";
 import editorConfigModule from "./editorconfig.ts";
-import gitModule from "../linters/git.ts";
-import glueModule from "../linters/glue.ts";
-import licenseModule from "../linters/license.ts";
-import prettierModule from "../linters/prettier.ts";
+import gitModule from "./git.ts";
+import glueModule from "./glue.ts";
+import licenseModule from "./license.ts";
+import prettierModule from "./prettier.ts";
 
 export const foxLinterModules = [
 	bakeModule,
 	basaltModule,
+	bashModule,
 	denoModule,
 	editorConfigModule,
 	gitModule,
