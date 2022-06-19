7181598e3e2efeaaefe909eb348cda578ba26aa7
Edwin Kofler <edwin@kofler.dev>
2022-06-18 21:12:10 -0700

    change: Use Cliffy for command line parsing

--- src/deps.ts
+++ src/deps.ts
@@ -7,6 +7,7 @@ export * as yaml from "https://deno.land/std@0.144.0/encoding/yaml.ts";
 export * as asserts from "https://deno.land/std@0.144.0/testing/asserts.ts";
 export * as buffer from "https://deno.land/std@0.144.0/io/buffer.ts";
 export * as conversion from "https://deno.land/std@0.144.0/streams/conversion.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 
 export { default as Ajv } from "https://esm.sh/ajv@8.11.0";
 export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,40 @@
-import { flags } from "./deps.ts";
+import { Command } from "./deps.ts";
 
-import * as types from "./types.ts";
-import * as util from "./util/util.ts";
-import * as helper from "./util/helper.ts";
 import { foxInit } from "./subcommands/init.ts";
 import { foxLint } from "./subcommands/lint.ts";
 import { foxDocs } from "./subcommands/docs.ts";
 import { foxRelease } from "./subcommands/release.ts";
 
-await main();
-async function main() {
-	const args = flags.parse(Deno.args);
-	if ("h" in args || "help" in args) {
-		util.showHelp();
-		Deno.exit(0);
-	}
-
-	const subcommand = args._[0];
-	switch (subcommand) {
-		case "init": {
-			await foxInit();
-			break;
-		}
-		case "lint": {
-			const f = util.validateFlags(args);
-
-			await foxLint(f);
-			break;
-		}
-		case "docs": {
-			await foxDocs();
-			break;
-		}
-		case "release": {
-			await foxRelease();
-			break;
-		}
-		case "":
-			util.showHelp();
-			util.die("Must supply a subcommand");
-			break;
-		default:
-			util.showHelp();
-			util.die("Subcommand not found");
-	}
-}
+await new Command()
+	.name("foxxy")
+	.description("My task automator")
+	.version("v0.1.0")
+	.command("init", "Initializes")
+	.action(async (obj) => {
+		console.log("init", obj);
+		await foxInit();
+	})
+	.reset()
+	.command("lint", "Lints")
+	.option("--fix", "Fix mistakes")
+	.action(async (obj) => {
+		console.log("lints", obj);
+		await foxLint(obj);
+	})
+	.reset()
+	.command("docs", "Generate documentation")
+	.action(async (obj) => {
+		console.log("docs", obj);
+		await foxDocs();
+	})
+	.reset()
+	.command("release", "Releases")
+	.action(async (obj) => {
+		console.log("docs", obj);
+		await foxRelease();
+	})
+	.reset()
+	.action(function () {
+		this.showHelp();
+	})
+	.parse(Deno.args);
