95b2f4a584ace3e11cc8ebd090c48f55fe864316
Edwin Kofler <edwin@kofler.dev>
2022-06-18 20:48:26 -0700

    chore: Update to std@0.144

--- src/deps.ts
+++ src/deps.ts
@@ -1,12 +1,12 @@
-export * as flags from "https://deno.land/std@0.125.0/flags/mod.ts";
-export * as c from "https://deno.land/std@0.125.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.125.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.125.0/path/mod.ts";
-export * as toml from "https://deno.land/std@0.125.0/encoding/toml.ts";
-export * as yaml from "https://deno.land/std@0.125.0/encoding/yaml.ts";
-export * as asserts from "https://deno.land/std@0.125.0/testing/asserts.ts";
-export * as buffer from "https://deno.land/std@0.125.0/io/buffer.ts";
-export * as conversion from "https://deno.land/std@0.125.0/streams/conversion.ts";
+export * as flags from "https://deno.land/std@0.144.0/flags/mod.ts";
+export * as c from "https://deno.land/std@0.144.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.144.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.144.0/path/mod.ts";
+export * as toml from "https://deno.land/std@0.144.0/encoding/toml.ts";
+export * as yaml from "https://deno.land/std@0.144.0/encoding/yaml.ts";
+export * as asserts from "https://deno.land/std@0.144.0/testing/asserts.ts";
+export * as buffer from "https://deno.land/std@0.144.0/io/buffer.ts";
+export * as conversion from "https://deno.land/std@0.144.0/streams/conversion.ts";
 
 export { default as Ajv } from "https://esm.sh/ajv@8.11.0";
 export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
