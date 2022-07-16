aff54f31461c43f65f5927315e2a89262e5ee417
Edwin Kofler <edwin@kofler.dev>
2022-07-15 22:10:46 -0700

    meta: Improve build and bundle commands

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,10 +1,20 @@
 # shellcheck shell=bash
 
 task.run() {
-	deno run --allow-read --allow-write --allow-net ./src/main.ts "$@"
+	deno run --unstable --allow-read --allow-write --allow-net ./src/main.ts "$@"
+}
+
+task.run-build() {
+	bake.cfg 'big-print' 'no'
+
+	deno run --unstable --allow-read --allow-write --allow-net ./output/bundle.js "$@"
+}
+
+task.watch() {
+	find . | entr -c -dd ./bake bundle
 }
 
 task.bundle() {
 	mkdir -p ./output
-	deno bundle ./src/main.ts ./output/bundle.js
+	deno bundle --unstable ./src/main.ts ./output/bundle.js
 }
